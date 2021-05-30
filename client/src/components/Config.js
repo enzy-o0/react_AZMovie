ebb50eeafb6ab7b7d83309be031c230051984771
ang_zy <oej3659@daum.net>
2021-05-30 21:37:55 +0900

    remove files

--- client/src/components/Config.js
+++
@@ -1,6 +0,0 @@
-//SERVER ROUTES
-export const USER_SERVER = '/api/users';
-
-export const API_KEY = '';
-export const API_URL = 'https://api.themoviedb.org/3/';
-export const API_IMAGE_URL = 'http://image.tmdb.org/t/p/';
